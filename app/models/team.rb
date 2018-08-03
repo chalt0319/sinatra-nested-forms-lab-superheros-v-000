cd788189af67262f148fdd3bad559d27c40faa1d
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-03 15:52:32 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,12 +1,11 @@
 class Team
-  attr_accessor :name, :power, :bio
+  attr_accessor :name, :motto
 
   @@all = []
 
   def initialize(params)
     @name = params[:name]
-    @power = params[:power]
-    @bio = params[:bio]
+    @motto = params[:motto]
     @@all << self
   end
 
