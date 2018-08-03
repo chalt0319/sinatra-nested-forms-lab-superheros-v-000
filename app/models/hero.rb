ed52aba057ba85487f6a06377f6559a068b91200
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-03 15:36:28 +0000

    automatically backed up by learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,8 +2,11 @@ class Hero < ActiveRecord::Base
   attr_accessor :name, :motto
   belongs_to :team
 
+  @@all = []
+  
   def initialize(params)
     @name = params[:name]
     @motto = params[:motto]
+    @@all << self 
   end 
 end
