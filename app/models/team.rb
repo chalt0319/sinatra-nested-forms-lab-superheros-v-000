91c8aa930f983f3a901b369f79ae8f2db0ac1d5d
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-03 15:30:42 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,9 @@
 class Team < ActiveRecord::Base
   attr_accessor :name, :power, :bio
   has_many :heros
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
 end
